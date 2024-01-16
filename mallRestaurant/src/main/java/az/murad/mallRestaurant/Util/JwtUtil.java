0182e8117273ec2391f3fb056e2d9f34fa1ed3ac
MuradIbrahimov <muradibrahimm@yahoo.com>
2024-01-17 01:11:06 +0400

    JwtUtil changed

--- mallRestaurant/src/main/java/az/murad/mallRestaurant/Util/JwtUtil.java
+++ mallRestaurant/src/main/java/az/murad/mallRestaurant/Util/JwtUtil.java
@@ -3,9 +3,11 @@ package az.murad.mallRestaurant.Util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
@@ -17,16 +19,14 @@ public class JwtUtil {
     @Value("${jwt.expirationMs}")
     private long expirationMs;
 
-    public String generateToken(String username, String role) {
-        Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + expirationMs);
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
+    public String generateToken(String username, String password) {
         return Jwts.builder()
                 .setSubject(username)
-                .claim("role", role)
-                .setIssuedAt(now)
-                .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, secret)
+                .claim("password", password)
+                .setIssuedAt(new Date())
+                .signWith(secretKey, SignatureAlgorithm.HS512)
                 .compact();
     }
 
